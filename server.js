ef9c1bffb095af8393432173b9af2ebe0f16381a
unknown <neelamnaveencs@gmail.com>
2021-06-04 14:20:08 +0530

    Deployable app to heroku

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.post("/addCustomer", function(req, res) {
     res.redirect('..');
 });
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static('client/build'));
+    app.get("*",(req, res) =>{
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(port, function() {
     console.log("express is running");
 })
